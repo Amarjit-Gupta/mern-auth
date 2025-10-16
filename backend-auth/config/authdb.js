1a1d855c253608c78dbe038df3003f0fd0d8e179
Amarjit-Gupta <akg433787@gmail.com>
2025-10-16 11:27:43 +0530

    first-commit

--- backend-auth/config/authdb.js
+++ backend-auth/config/authdb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 if(!mongoose.connection.readyState){
-    mongoose.connect(process.env.DATABASE_URI).then(()=>{
+    mongoose.connect("mongodb+srv://guptaamarjit777:4CimwemWzP2chirV@my-cluster.pz0rn.mongodb.net/mern-project").then(()=>{
         console.log("database connected..");
     }).catch((err)=>{
         console.log("database connection failed...");
--- frontend/src/URL.jsx
+++ frontend/src/URL.jsx
@@ -1,2 +1,4 @@
-export const AUTHURL = "http://localhost:4000";
+// export const AUTHURL = "http://localhost:4000";
+export const AUTHURL = "https://mern-auth-psi-three.vercel.app";
+
 export const DATAURL = "http://localhost:5000";
