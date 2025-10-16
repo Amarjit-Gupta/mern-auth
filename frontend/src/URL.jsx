340615fc79551e8a210a1fc687cf65d59d106c2a
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2025-10-16 11:46:36 +0530

    Update URL.jsx

--- frontend/src/URL.jsx
+++ frontend/src/URL.jsx
@@ -1,4 +1,5 @@
 // export const AUTHURL = "http://localhost:4000";
-export const AUTHURL = "https://mern-auth-psi-three.vercel.app";
+// export const AUTHURL = "https://mern-auth-beryl-chi.vercel.app";
 
-export const DATAURL = "http://localhost:5000";
+export const AUTHURL = "https://mern-auth-beryl-chi.vercel.app";
+export const DATAURL = "https://mern-auth-data.onrender.com";
